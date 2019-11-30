e9b47a888b92ef58dca98011f65185a81f9f9303
zhangliGit <674937076@qq.com>
2019-11-30 21:44:05 +0800

    init code

--- .cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---node-modules-antdsite-src-templates-index-tsx": () => import("../node_modules/antdsite/src/templates/index.tsx" /* webpackChunkName: "component---node-modules-antdsite-src-templates-index-tsx" */),
-  "component---cache-dev-404-page-js": () => import("dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */)
-}
-
